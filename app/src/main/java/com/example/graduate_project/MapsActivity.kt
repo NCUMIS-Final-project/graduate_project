6a8db1250254a99ec7761816e17cc982c3624eee
TehTing <gina2081731016@gmail.com>
2022-06-30 01:00:15 +0800

    Google map 可取得使用者位置

--- app/src/main/java/com/example/graduate_project/MapsActivity.kt
+++ app/src/main/java/com/example/graduate_project/MapsActivity.kt
@@ -1,48 +1,129 @@
 package com.example.graduate_project
 
+/** https://firebase.google.com/docs/database/android/read-and-write?hl=zh-cn#kotlin+ktx_1
+ *  Firebase 讀資料方法
+ *
+ *  https://www.youtube.com/watch?v=FotQIcC91V4&ab_channel=CodeStance
+ *  Google map 設定目前位置
+ */
+
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.example.graduate_project.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object{
+        private const val LOCATION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //setContentView(R.layout.activity_maps)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setOnMarkerClickListener(this)
+        setUpMap()
+
+        /*       val data = arrayOf(
+                   arrayOf("24.9660","121.1960","IKEA"),
+                   arrayOf("24.9700","121.1940","星巴克")
+               )
+
+               //利用陣列來加小紅點。
+               for (i in data.indices) {
+                   var dbelat = 0.0
+                   var dbelng = 0.0
+                   try {
+                       dbelat = data[i][0]!!.trim().toDouble()
+                       dbelng = data[i][1]!!.trim().toDouble()
+
+                       //宣告MarkerOptions
+                       val objoption = LatLng(dbelat, dbelng)
+
+                       //加入Google Map，並且設定標題
+                       mMap.addMarker(MarkerOptions().position(objoption).title(data[i][2]))
+                   } catch (e: NullPointerException) {
+                   }
+               }
+
+               //宣告MarkerOptions
+               val objstartpoint = LatLng(24.9683, 121.1953)
+
+               //加入Google Map，並且設定標題
+               mMap.addMarker(MarkerOptions().position(objstartpoint).title("中央大學"))
+
+               //設定 左上角的指南針，要兩指旋轉才會出現
+               mMap.uiSettings.isCompassEnabled = true
+
+               //設定 右下角的導覽及開啟 Google Map功能
+               mMap.uiSettings.isMapToolbarEnabled = true
+
+               //移動地圖到那個座標
+               mMap.moveCamera(CameraUpdateFactory.newLatLng(objstartpoint))
+
+               //放大地圖到15倍
+               mMap.animateCamera(CameraUpdateFactory.zoomTo(15f))
+               */
     }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_REQUEST_CODE)
+            return
+        }
+        mMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this){ location ->
+            if (location != null){
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+            }
+
+        }
+    }
+
+    private fun placeMarkerOnMap(currentLatLong: LatLng) {
+        val markerOptions = MarkerOptions().position(currentLatLong)
+        markerOptions.title("$currentLatLong")
+        mMap.addMarker(markerOptions)
+    }
+
+    override fun onMarkerClick(p0: Marker?) = false
 }
